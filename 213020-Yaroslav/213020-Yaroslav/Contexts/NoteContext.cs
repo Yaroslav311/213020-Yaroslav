4c7cd03021b69e4dff2a14298b128ef9fb20c000
Ler4i <valeriazotova553@gmail.com>
2021-10-02 17:55:32 +0300

    База данных и тп

---
+++ 213020-Yaroslav/213020-Yaroslav/Contexts/NoteContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using  _213020_Yaroslav.Models;
+
+namespace _213020_Yaroslav.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
+
---
+++ 213020-Yaroslav/213020-Yaroslav/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _213020_Yaroslav.Models
+{
+    public class Note
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? ChangeDate { get; set; }
+
+
+    }
+}
